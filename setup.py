81d98a6d55dc88756e6a4ac65d6b440f52e8ce47
Brian Balsamo <balsamo@uchicago.edu>
2016-03-01 10:03:09 -0600

    first commit moving code over from uchicagoldr-primary

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'uchicagoldr',
+    version = '1.0.0',
+    author = "Brian Balsamo",
+    author_email = ["balsamo@uchicago.edu"],
+    packages = ['hierarchicalrecord'],
+    description = "A library provided easy access to a hierarchical record data organization",
+    keywords = ["uchicago","repository","records"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+    )
