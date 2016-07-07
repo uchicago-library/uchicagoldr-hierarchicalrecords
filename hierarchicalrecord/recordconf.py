a6e34f68fef17239a7fe171d15a2aaeaf3ac582c
Brian Balsamo <balsamo@uchicago.edu>
2016-07-07 11:45:56 -0500

    Added some more serializations for confs.

--- hierarchicalrecord/recordconf.py
+++ hierarchicalrecord/recordconf.py
@@ -1,4 +1,5 @@
-from csv import DictReader
+from csv import DictReader, DictWriter
+from json import loads, dumps
 
 class RecordConf(object):
 
@@ -18,7 +19,25 @@ class RecordConf(object):
         with open(csv_filepath, 'r') as f:
             reader = DictReader(f)
             rows = [row for row in reader]
-        self.data = rows
+        for x in rows:
+            self.data.append(x)
 
+    def to_csv(self, csv_filepath):
+       field_names = ["Field Name", "Value Type", "Obligation", "Cardinality",
+                      "Validation", "Children Required"]
+       w = DictWriter(csv_filepath, fieldnames=field_names)
+       w.writeheader()
+       for x in self.data:
+           w.writerow(x)
+
+    def from_json(self, json_filepath):
+        with open(json_filepath, 'r') as f:
+            for line in f.readlines():
+                self.data.append(loads(line.rstrip("\n")))
+
+    def to_json(self, json_filepath):
+        with open(json_filepath, 'w') as f:
+            for x in self.data:
+                f.write(dumps(x)+"\n")
 
     data = property(get_data, set_data)
